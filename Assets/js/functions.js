06c5adc70cff825cc8c6a7a2b3f1c9a4dd61fd16
JOR4M0519 <jdramoss@unbosque.edu.co>
2022-03-10 23:49:12 -0500

    Final Version

--- Assets/js/functions.js
+++ Assets/js/functions.js
@@ -1,18 +1,22 @@
-const url = 'http://localhost:3000/NEAr';
+const url = "http://localhost:3000/NEAr";
 let campo = document.querySelector(".contenedor-social");
-console.log(campo);
-//
-var contador = 0;
-var likes = 0;
 
-function contar() {
+async function contar(id, currentLikes) {
   console.log("entra");
-  document.getElementById("btnlikes").innerHTML = contador++;
-  if (contador > 10) {
-    contador++;
-  }
-  let msg = "Llevas " + contador + "likes, no puedes dar mas de 10";
-  alert(msg);
+  var likes = currentLikes + 1;
+  let resp = await fetch(`${url}/${id}`, {
+    method: "PATCH",
+    body: JSON.stringify({
+      likes,
+    }),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  });
+
+  let data = resp.json();
+  console.log(data);
+  location.reload();
 }
 
 const getData = async () => {
@@ -20,17 +24,17 @@ const getData = async () => {
   const data = await resp.json();
   console.log(data);
   data.forEach((card) => {
-    const { nombre, foto, coleccion, precio, contador, nickname } = card;
+    const { id, name, photo, colection, price, likes, nickname } = card;
     campo.innerHTML += ` 
     <div class="card">
-    <img src="${foto}" class="card-img-top"
+    <img src="${photo}" class="card-img-top"
         alt="...">
     <div class="card-body">
-        <h5 class="card-title">${precio} </h5>
-        <p class="card-text">${nombre} &#8226; ${coleccion} </p>
+        <h5 class="card-title">${price} </h5>
+        <p class="card-text">${name} &#8226; ${colection} </p>
         <p>${nickname}</p>
-        <p>${contador} likes</p>
-        <button onclick="contar()" type="button" class="btn btn-outline-ligth-grey btn1" id="btnlikes"><span class="fa fa-heart"></span></button>
+        <p>${likes} likes</p>
+        <button onclick="contar(${id}, ${likes})" type="button" class="btn btn-outline-ligth-grey btn1" id="btnlikes"><span class="fa fa-heart"></span></button>
     </div>
 </div>
         `;
