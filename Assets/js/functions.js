2ca2d4aea8311ad18ce463a6c3a5439ac7d8dcb7
azptmdr <ksgarciap@unbosque.edu.co>
2022-03-10 20:24:51 -0500

    Final

--- Assets/js/functions.js
+++ Assets/js/functions.js
@@ -3,14 +3,16 @@ let campo = document.querySelector(".contenedor-social");
 console.log(campo);
 //
 var contador = 0;
-let msg = "Solo puedes dar un clic por obra de arte";
+var likes = 0;
+
 function contar() {
   console.log("entra");
   document.getElementById("btnlikes").innerHTML = contador++;
-  if (contador > 1) {
-    contador = 1;
-    alert(msg);
+  if (contador > 10) {
+    contador++;
   }
+  let msg = "Llevas " + contador + "likes, no puedes dar mas de 10";
+  alert(msg);
 }
 
 const getData = async () => {
@@ -18,15 +20,19 @@ const getData = async () => {
   const data = await resp.json();
   console.log(data);
   data.forEach((card) => {
-    const { nombre, foto, coleccion, precio, likes, nickname } = card;
+    const { nombre, foto, coleccion, precio, contador, nickname } = card;
     campo.innerHTML += ` 
-        <div class="card">
-        <img src="${foto}" alt="">
-        <p>${nombre}</p>
-        <p style="color:blue; font-weigth:bold">${nickname}</p>
-        <p>${precio}</p>
-        <p>"Colección: "${coleccion}</p>
-        <button id="btnlikes" class="btn btn-primary" onclick="contar()">${likes} Like </button>
+    <div class="card">
+    <img src="${foto}" class="card-img-top"
+        alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${precio} </h5>
+        <p class="card-text">${nombre} &#8226; ${coleccion} </p>
+        <p>${nickname}</p>
+        <p>${contador} likes</p>
+        <button onclick="contar()" type="button" class="btn btn-outline-ligth-grey btn1" id="btnlikes"><span class="fa fa-heart"></span></button>
+    </div>
+</div>
         `;
   });
 };
